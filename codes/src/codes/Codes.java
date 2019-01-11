adb01febf9eed71df260cab4ed2548ad6120194f
gotenks274 <leminhtri2704.lmt@gmail.com>
2019-01-11 16:59:40 +0700

    first version

---
+++ codes/src/codes/Codes.java
@@ -0,0 +1,17 @@
+
+package codes;
+
+/**
+ *
+ * @author lemin
+ */
+public class Codes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
