9edb5ae254ee5c3ac35105d98a9b9ea8381fa623
gintoki2000 <ngotrungaccor@gmail.com>
2020-01-11 16:48:07 +0700

    add setter for x
    add getter for x

--- codes/src/codes/Codes.java
+++ codes/src/codes/Codes.java
@@ -8,6 +8,7 @@ package codes;
 public class Codes {
 
     int x;
+
     /**
      * @param args the command line arguments
      */
@@ -16,6 +17,18 @@ public class Codes {
         System.out.println("Hello guys");
         System.out.println("123");
     }
-    
 
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
 }
